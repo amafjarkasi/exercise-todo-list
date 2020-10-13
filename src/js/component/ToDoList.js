99434ebe9537e2ec559f835c1728753e9a1dab9b
amafjarkasi <amafjarkasi@gmail.com>
2020-10-13 01:05:52 +0000

    new changes

--- src/js/component/ToDoList.js
+++ src/js/component/ToDoList.js
@@ -1,8 +1,20 @@
 import React from "react";
 
 class ToDoList extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			tasks: []
+		};
+	}
 	render() {
 		return <div>Hello World</div>;
+	}
+	addTask() {
+		console.log("addTask");
+	}
+	removeTask() {
+		console.log("removeTask");
 	}
 }
 
