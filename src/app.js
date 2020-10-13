3a68aed60051f1007b728d883abeabbd38fcedc2
amafjarkasi <amafjarkasi@gmail.com>
2020-10-13 00:47:50 +0000

    new changes

---
+++ src/app.js
@@ -0,0 +1,50 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+class Counter extends React.Component {
+    constructor(props){
+        super(props);
+        this.handleOne = this.handleOne.bind(this);
+        this.handleMinusOne = this.handleMinusOne.bind(this);
+        this.handleReset = this.handleReset.bind(this);
+        this.state = {
+            count : 0,
+        }
+    }
+    handleOne(){
+        this.setState(( prevState) => {
+            return {
+                count : prevState.count + 1
+            }
+        });
+    }
+    handleMinusOne(){
+        this.setState(( prevState) => {
+            return {
+                count : prevState.count -1
+            }
+        });
+    }
+
+    handleReset(){
+        this.setState(() => {
+            return {
+                count : 0
+            }
+        });
+    }
+
+    render(){
+        return (
+            <div>
+                <h1>Count: {this.state.count}</h1>
+                <button onClick={this.handleOne}>+1</button>
+                <button onClick={this.handleMinusOne}>-1</button>
+                <button onClick={this.handleReset}>reset</button>
+            </div>
+        )
+    }
+}
+
+
+ReactDOM.render(<Counter />, document.getElementById("app"));
---
+++ src/js/component/ToDoList.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class ToDoList extends React.Component {
+	render() {
+		return <div>Hello World</div>;
+	}
+}
+
+export { ToDoList as default };
